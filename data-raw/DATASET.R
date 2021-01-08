368ab68c4065b12527f8f5caf93395381c863ef2
Gregory Jefferis <jefferis@gmail.com>
2021-01-08 10:24:07 +0000

    Add MANC as alias for JRCFIBVNC2020

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -5,3 +5,7 @@ library(nat)
 JRCFIBVNC2020MNP.surf <- read.hxsurf('data-raw/VNC_tbars_scale-5.resampled.500k.bin.surf')
 
 usethis::use_data(JRCFIBVNC2020MNP.surf, overwrite = TRUE)
+
+# alias - easiest way just to make a copy sadly
+MANC.surf <- JRCFIBVNC2020MNP.surf
+usethis::use_data(MANC.surf, overwrite = TRUE)
