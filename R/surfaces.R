eec19509e54493812364ffd3d7ee8f07c73e291c
Gregory Jefferis <jefferis@gmail.com>
2021-01-08 12:30:38 +0000

    document surface object

---
+++ R/surfaces.R
@@ -0,0 +1,17 @@
+#' Surface model of neuropils in the MANC dataset
+#'
+#' The \code{\link{MANC.surf}} model was generated in Amira by Kazunori Shinomiya and shared with Greg 1 October 2020. It should be regarded as provisional
+
+#'
+#'
+#'
+#' @name MANC.surf
+#' @aliases JRCFIBVNC2020MNP.surf
+#' @docType data
+#' @examples
+#' \dontrun{
+#' # Depends on nat
+#' library(nat)
+#' plot3d(MANC.surf)
+#' }
+NULL
