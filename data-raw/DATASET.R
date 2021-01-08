98d68586e0834aebe34c510b2040e7eece6b627c
Gregory Jefferis <jefferis@gmail.com>
2021-01-08 08:56:59 +0000

    correct male vnc surface script

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -4,4 +4,4 @@ library(nat)
 
 JRCFIBVNC2020MNP.surf <- read.hxsurf('data-raw/VNC_tbars_scale-5.resampled.500k.bin.surf')
 
-usethis::use_data(DATASET, overwrite = TRUE)
+usethis::use_data(JRCFIBVNC2020MNP.surf, overwrite = TRUE)
