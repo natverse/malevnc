60dd69d484cbeb1ab41a1f4816fc33f6ca4083fd
Gregory Jefferis <jefferis@gmail.com>
2021-01-29 11:42:12 +0000

    Add mirroring registrations for MANC
    * mirror_manc to mirror points
    * symmetric_manc to convert to a symmetric space

---
+++ R/xform.R
@@ -0,0 +1,120 @@
+#' Mirror points or other 3D objects along the MANC midline
+#'
+#' @details These registration functions depend on an installation of the CMTK
+#'   registration toolkit. See \code{\link[nat]{cmtk.bindir}} for details.
+#' @param x 3D vertices (or object containing them) in MANC microns. Could be \code{\link{neuron}}, \code{\link{neuronlist}}, \code{\link{hxsurf}} etc.
+#' @param level Which level of the CMTK registrations to use (for expert/testing
+#'   purposes only)
+#' @param ... additional arguments passed to xform and friends
+#'
+#' @return Transformed points/object
+#' @export
+#'
+#' @examples
+#' \donttest{
+#' # NB convert from raw coordinates to microns on input
+#' hookr=cbind(33549, 45944, 50806)*c(8,8,8)/1000
+#' hookl=cbind(8718, 40794, 52140)*c(8,8,8)/1000
+#' hookrl=rbind(hookr, hookl)
+#'
+#' # mirror these positions
+#' hookrl.m=mirror_manc(hookrl)
+#' hookrl.m
+#' # compute difference from
+#' # transformed L to original R
+#' # transformed R to original L
+#' # distances are 1-8 microns in each axis, due to placement variations.
+#' hookrl.m-hookrl[2:1, ]
+#' }
+#' \dontrun{
+#' wire3d(MANC.surf, col='grey', lwd=.5)
+#' points3d(hookrl, col=c("green", "red"), size=10)
+#' points3d(hookrl.m, col=c("green", "red"), size=5)
+#' # the lines will link the transformed points
+#' segments3d(rbind(hookrl, hookrl.m)[c(1,3,2,4), ])
+#'
+#' # show the original surface and the mirrored surface
+#' MANC.surf.m=mirror_manc(MANC.surf)
+#' MANC.surf.m=mirror_manc(MANC.surf)
+#' nclear3d()
+#' plot3d(MANC.surf)
+#' wire3d(MANC.surf.m)
+#' }
+#'
+#' @importFrom nat reglist xform invert_reglist
+#' @importFrom nat.templatebrains as.templatebrain mirror_brain
+mirror_manc <- function(x, level=c(5,4), ...) {
+
+  mirror_reg_f=mirror_manc_reglist(level = level)
+  mirror_reg_r=mirror_manc_reglist("reverse", level = level)
+  xt=xform(x, reg=mirror_reg_f, ... )
+  xtm=mirror_brain(xt, brain = MANCsym, mirrorAxis = 'X', transform='flip')
+  xtmt=xform(xtm, reg=mirror_reg_r, ... )
+  xtmt
+}
+
+#' @rdname mirror_manc
+#' @description \code{symmetric_manc} transforms neurons, surfaces and other
+#'   point data onto a symmetrised version of the MANC template brain,
+#'   optionally mirroring across the midline.
+#' @param mirror Whether to mirror across the midline when using
+#'   \code{symmetric_manc}
+#' @examples
+#' \donttest{
+#' symmetric_manc(MANC.surf)
+#' # mirror across the midline in symmetric space also
+#' MANCsym.surf.m=symmetric_manc(MANC.surf, mirror=T)
+#'
+#' plot3d(MANCsym.surf)
+#' wire3d(MANCsym.surf.m)
+#' }
+symmetric_manc <- function(x, level=c(5,4), mirror=FALSE, ...) {
+
+  mirror_reg_f=mirror_manc_reglist(level = level)
+  xt=xform(x, reg=mirror_reg_f, ... )
+  if(isTRUE(mirror))
+    xt=mirror_brain(xt, brain = MANCsym, mirrorAxis = 'X', transform='flip')
+  xt
+}
+
+
+# internal function to return a CMTK mirroring registration
+mirror_manc_reglist <- function(direction=c("forward", "reverse"), level=c(5,4)) {
+  if(length(level)>1) level=level[1]
+  level=checkmate::asInt(5.0, lower=4L, upper = 5L)
+  direction=match.arg(direction)
+  pkg = utils::packageName()
+  f1 = system.file(
+    sprintf(
+      "inst/reg/mancsym-4_vnc-tbar-vol-512nm-g1-flip_01_warp_m0g80c8e1e-1x26r4.list/level-0%d.list",level),
+    package = pkg
+  )
+  f2=system.file("inst/reg/mancsym-4-flip_mancsym-4-halved.list", package = pkg)
+  # nb in CMTK direction is defined by the image transform which is the opposite
+  # of the points transform, hence swap=T
+  mirror_reg_f <- reglist(nat::cmtkreg(f1), nat::cmtkreg(f2), swap = c(T,T))
+  if(direction=='forward') mirror_reg_f else invert_reglist(mirror_reg_f)
+}
+
+MANCsym = structure(
+  list(
+    name = "mancsym-4-symmetric",
+    regName = "MANCsym",
+    type = "Synthetic average brain from T-bar predictions based on MANC FIB sem data",
+    sex = "M",
+    dims = c(672L, 830L, 1280L),
+    voxdims = c(0.512,
+                0.512, 0.512),
+    origin = c(0, 0, 0),
+    BoundingBox = structure(
+      c(0,
+        343.552, 0, 424.448, 0, 654.848),
+      .Dim = 2:3,
+      class = "boundingbox"
+    ),
+    units = NULL,
+    description = NULL,
+    doi = NULL
+  ),
+  class = "templatebrain"
+)
