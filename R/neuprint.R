d2656a744dbcef41e26ac7238a7caa39c8b35d8f
Gregory Jefferis <jefferis@gmail.com>
2021-04-16 09:44:37 +0000

    Authenticate to neuprint-pre vnc server

---
+++ R/neuprint.R
@@ -0,0 +1,22 @@
+#' Login to MANC neuprint server
+#'
+#' @param ... Additional arguments passed to \code{\link{neuprint_login}}
+#'
+#' @return a \code{\link{neuprint_connection}} object returned by \code{\link{neuprint_login}}
+#' @export
+#'
+#' @examples
+#' \donttest{
+#' vncc=manc_neuprint()
+#' anchorids <- neuprintr::neuprint_ids("status:Anchor", conn=vncc)
+#' # the last connection will be used by default
+#' anchormeta <- neuprintr::neuprint_get_meta("status:Anchor")
+#'
+#' plot(cumsum(sort(anchormeta$pre, decreasing = T)), ylab='Cumulative presynapses')
+#' plot(cumsum(sort(anchormeta$post, decreasing = T)), ylab='Cumulative postsynapses')
+#'
+#' }
+manc_neuprint <- function(...) {
+  neuprintr::neuprint_login(server='https://neuprint-pre.janelia.org', dataset = "vnc", token=Sys.getenv("neuprint_token"), ...)
+}
+
