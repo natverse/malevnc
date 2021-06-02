natverse/malevnc

commit b49142bab216ca294495efc68a764ceda52ecaeb

manc_body_annotations can't update ids without position

R/annotations.R
@@ -262,8 +262,10 @@ manc_body_annotations <- function(ids=NULL, query=NULL, json=FALSE, config=NULL,
 }
 
 updatebodyids <- function(x, update=TRUE, cache=FALSE) {
-  if(isFALSE(update)) return(x)
-  stopifnot(is.data.frame(x) && all(c("bodyid", "position") %in% colnames(x)))
+  # we can't do anything if we don't have position info
+  if(isFALSE(update) || !isTRUE("position" %in% names(x)))
+    return(x)
+  stopifnot(is.data.frame(x) && !"bodyid" %in% colnames(x))
   xyz=xyzmatrix(x$position)
   goodpos=!is.na(xyz[,1])
   x$original.bodyid=x$bodyid

commit 01e0f0ce90ec8af48c8344cec0c7d8b57809284a

fix bug introduced in b49142b for updatebodyids/manc_body_annotations

R/annotations.R
@@ -265,7 +265,8 @@ updatebodyids <- function(x, update=TRUE, cache=FALSE) {
   # we can't do anything if we don't have position info
   if(isFALSE(update) || !isTRUE("position" %in% names(x)))
     return(x)
-  stopifnot(is.data.frame(x) && !"bodyid" %in% colnames(x))
+  if(!is.data.frame(x) || (!"bodyid" %in% colnames(x)))
+    stop("Must have a data.frame with a bodyid column!")
   xyz=xyzmatrix(x$position)
   goodpos=!is.na(xyz[,1])
   x$original.bodyid=x$bodyid