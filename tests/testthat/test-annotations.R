1a45603f46eae61288422687ac7c83404aa8a072
Dominik Krzeminski <dkk33@cam.ac.uk>
2021-09-01 15:25:40 +0100

    fixed gh build by skipping annotation test

--- tests/testthat/test-annotations.R
+++ tests/testthat/test-annotations.R
@@ -1,6 +1,8 @@
 context("annotations")
 
 test_that("manc_meta", {
+  skip_if(inherits(try(clio_token(), silent = T), 'try-error'),
+          message = "no clio token available")
   res <- manc_meta('Giant Fiber')
   expect_true(is.data.frame(res))
   expect_true("dvid_group" %in% colnames(res))
