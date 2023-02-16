57e9411b4b2027ad0568db728dc2984db7fbb610
Gregory Jefferis <jefferis@gmail.com>
2023-02-16 09:16:33 +0000

    correct svg test baseline

--- tests/testthat/test-svg.R
+++ tests/testthat/test-svg.R
@@ -13,5 +13,5 @@ test_that("multiplication works", {
   on.exit(unlink(tf))
   expect_silent(colour_leg_muscles(f=tf, colpal=pal))
   skip_if_not_installed('digest')
-  expect_equal(digest::digest(file = tf), 'c4ea382ca385c24f8e83c8ce27b3588b')
+  expect_equal(digest::digest(file = tf), '952c5b53bd180df510115f33aabd5c2c')
 })
