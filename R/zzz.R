a39586a3c5ffb0c6ae744924d32724d8147c94fe
Gregory Jefferis <jefferis@gmail.com>
2023-07-03 17:00:35 +0100

    don't allow onLoad to error

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,5 @@
 .onLoad <- function(libname, pkgname) {
-  choose_malevnc_dataset(dataset = getOption("malevnc.dataset"), set=TRUE)
+  try(choose_malevnc_dataset(dataset = getOption("malevnc.dataset"), set=TRUE))
 
   # TODO make MANC<->manc/JRCVNC2018U bridging registrations available
 
