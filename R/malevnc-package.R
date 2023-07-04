5fad5b1400e2bc1361886de81812352392e1e80a
Gregory Jefferis <jefferis@gmail.com>
2023-07-04 16:33:54 +0100

    Add info about registration to package docs

--- R/malevnc-package.R
+++ R/malevnc-package.R
@@ -1,5 +1,12 @@
 #' @keywords internal
 #' @import nat
+#' @section Registration: For information about left-right mirroring and
+#'   symmetrising registrations within MANC, please see
+#'   \code{\link{mirror_manc}}.
+#'
+#'   For information about across-template registrations, please see
+#'   \code{\link{download_manc_registrations}}.
+#'
 #' @section Package Options: \itemize{
 #'
 #'   \item \code{malevnc.server} The https URL to the main Janelia server
