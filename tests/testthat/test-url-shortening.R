317ffcb9ad99c72843ba8e8eb9620c76436b5458
Gregory Jefferis <jefferis@gmail.com>
2025-03-05 15:01:06 +0000

    can no longer test MD5 shortening method
    * new link shortnener prevents overwrites without a password

--- tests/testthat/test-url-shortening.R
+++ tests/testthat/test-url-shortening.R
@@ -3,11 +3,5 @@ test_that("url shortening works", {
 
   expect_is(lu <- malevnc::flyem_expand_url('https://neuroglancer-demo.appspot.com/#!gs://flyem-user-links/short/2023-12-15.090703.json'), 'character')
 
-  expect_equal(su <- flyem_shorten_url(c(lu,lu),
-                                       title=c("test1", "test2"),
-                                       method = 'md5'),
-               c("https://neuroglancer-demo.appspot.com/#!gs://flyem-user-links/short/59a3c8a14ac42f1561713d5e3c609381.json",
-                 "https://neuroglancer-demo.appspot.com/#!gs://flyem-user-links/short/8e0d1284e3af583db48d92dc02280eea.json"
-               )
-  )
+  expect_length( flyem_shorten_url(c(lu,lu), title=c("test1", "test2")), 2)
 })
